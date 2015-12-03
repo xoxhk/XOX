3b020149489d4999a6f4169d52a5d34b7bac517a
xoxhk <xox@xox.hk>
2015-12-03 11:03:50 +0800

    test003

--- XOX/ViewController.swift
+++ XOX/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("dddd");
-        print("dddd");
+        print("ddddwwww");
     }
 
     override func didReceiveMemoryWarning() {
